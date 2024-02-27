22eda06f4f09ecd6930e07750438a3521bb70475
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2024-02-27 20:30:31 +0200

    create/view/delete robots attached to specific user

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PassportAuthController;
+use App\Http\Controllers\RobotController;
 use App\Http\Controllers\TestingController;
 
 /*
@@ -20,8 +21,15 @@ Route::post('register', [PassportAuthController::class, 'register']);
 Route::post('login', [PassportAuthController::class, 'login']);
 Route::post('test',  [TestingController::class, 'test']);
 
+
 Route::middleware(['auth:api'])->group(function () {
     Route::get('show', [PassportAuthController::class, 'show']);
     Route::put('update', [PassportAuthController::class, 'update']);
     Route::delete('delete', [PassportAuthController::class, 'delete']);
+
+    Route::post('createRobot',  [RobotController::class, 'create']);
+    Route::get('getRobots', [RobotController::class,'getRobots']);
+    Route::delete('deleteRobot', [RobotController::class,'deleteRobot']);
+
+
 });
