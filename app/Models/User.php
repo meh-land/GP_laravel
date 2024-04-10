a3b7d60a316ed76b82b0a50bacdace26578ea96b
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2024-04-10 15:09:27 +0200

    maps model and table created

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,4 +47,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Robot::class);
     }
+
+    public function maps()
+    {
+        return $this->hasMany(Maps::class);
+    }
 }
